bfe26c2237b324b9a1b1a507f31f04e0653c7d85
Fox Gardner <fox@gardnermedia.com>
2024-03-08 13:19:47 -0700

    Layout page

---
+++ myapp/src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<div id="site-background">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav mx-auto">
+                <a class="nav-link" href="/">Owl & Fox</a>
+                <a class="nav-link" aria-current="page" href="/">Home</a>
+                <a class="nav-link" href="/">Bureaucracy</a>
+                <a class="nav-link" href="/">Differential Equations</a>
+                <a class="nav-link"  href="/"aria-disabled="true">Gaming</a>
+            </div>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container content-baby">
+        <slot />
+    </div>
+</div>
